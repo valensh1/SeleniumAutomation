2cf680fdaa42ada61ab97741b9e7b0f061662a3b
shaunvalentine <valensh1@yahoo.com>
2024-08-30 12:09:58 -0700

    Completed Add Remove Elements feature

--- src/main/java/InternetHerokuApp/pages/AddRemoveElementsPage.java
+++ src/main/java/InternetHerokuApp/pages/AddRemoveElementsPage.java
@@ -21,21 +21,31 @@ public class AddRemoveElementsPage extends AddRemoveElementsPO {
         wait.until(ExpectedConditions.visibilityOf(driver.findElement(addRemoveElementsPageTitle)));
     }
 
-    public void clickButtonNumTimes(String button, int count) throws InterruptedException {
-        for (int i = 0; i < count; i++) {
+    public void clickButtonNumTimes(String button, int clickCount, int buttonCount)  {
+        while(clickCount > 0) {
             AppUtilities.clickElementByText(driver, button);
-                waitUntilElementVisible(i + 1);
+                waitUntilElementVisible(clickCount, buttonCount, button);
+                clickCount--;
         }
     }
 
-    public void waitUntilElementVisible(int count) {
+    public void waitUntilElementVisible(int clickCount, int buttonCount, String button) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(deleteButtonCollection));
+        if (clickCount == 1 && buttonCount == 0 && button.equalsIgnoreCase("delete")) {
+            wait.until(ExpectedConditions.numberOfElementsToBe(deleteButtonCollection, 0));
+        } else {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(deleteButtonCollection));
+        }
     }
 
     public void validateNumberOfButtonsDisplayed(int count) {
-        int buttonCount = driver.findElements(deleteButtonCollection).size();
-        System.out.println("The number of buttons that should be displayed is " + buttonCount + " and that compares to the actual of " + count);
-        assertThat(buttonCount).isEqualTo(count);
+        if(count == 0) {
+            int numOfElementsDisplayed = driver.findElements(deleteButtonCollection).size();
+            assertThat(numOfElementsDisplayed).isEqualTo(0);
+        } else {
+            int buttonCount = driver.findElements(deleteButtonCollection).size();
+            System.out.println("The number of buttons that should be displayed is " + buttonCount + " and that compares to the actual of " + count);
+            assertThat(buttonCount).isEqualTo(count);
+        }
     }
 }
